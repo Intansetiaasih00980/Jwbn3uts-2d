91704ceea69da27b3b0540b9a1c35e5437cf4d71
Intansetiaasih00980 <122811984+Intansetiaasih00980@users.noreply.github.com>
2023-01-17 11:55:58 +0700

    Initial commit

---
+++ src/Uastugas.java
@@ -0,0 +1,12 @@
+public class Uastugas {
+    public static void main(String[] args){
+    String kalimat = "halo selamat datang di my home";
+        for(int = 0; <kalimat.lenght(); i++) {
+            if(kalimat.charArt(i) == 'a' 'i' 'u' 'e' '0') {
+                System.out.println("*");
+                i++
+            }
+            System.out.println(kalimat.charArt(i));
+        }
+    }
+}
